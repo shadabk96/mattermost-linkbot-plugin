52c17a0898602114230eff42a9a24170686bd674
Shadab Khan <shadabk96@gmail.com>
2020-03-01 16:45:44 +0530

    Integrate

--- webapp/src/components/right_hand_sidebar/rhs_view.jsx
+++ webapp/src/components/right_hand_sidebar/rhs_view.jsx
@@ -14,14 +14,25 @@ import Button from '@material-ui/core/Button';
 
 import Typography from '@material-ui/core/Typography';
 
-export default function RHSView() {
+import axios from 'axios';
+
+import PropTypes from 'prop-types';
+
+const CustomCard = (props) => {
+    const maxWidthConst = 345;
+    const useStyles = makeStyles({
+        root: {
+            maxWidth: maxWidthConst,
+        },
+    });
+    const {test, data} = props;
+    console.log(test);
     const classes = useStyles();
-
-    const links = [{message: 'message1', tags: '#tag1', link: 'link1'}, {message: 'message2', tags: '#tag2', link: 'link2'}];
-
+    const links = data;
+    console.log(links);
     return (
-        <div style={style.rhs}>
-            {links.map((link) => {
+        <div>
+            {links.length === 0 ? (<div>{'Loading...'}</div>) : (links.map((link) => {
                 return (
 
                     <Card
@@ -62,21 +73,55 @@ export default function RHSView() {
                         </CardActions>
                     </Card>
                 );
-            })}
+            }))}
         </div>
     );
-}
-
-const style = {
-    rhs: {
-        padding: '10px',
-    },
 };
 
-const maxWidthConst = 345;
+CustomCard.propTypes = {
+    test: PropTypes.object,
+    data: PropTypes.object,
+};
 
-const useStyles = makeStyles({
-    root: {
-        maxWidth: maxWidthConst,
-    },
-});
+export default class RHSView extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            links: [],
+            loading: true,
+        };
+    }
+    componentDidMount() {
+        this.renderPosts();
+    }
+
+    renderPosts = async () => {
+        // const links = [{message: 'message1', tags: '#tag1', link: 'link1'}, {message: 'message2', tags: '#tag2', link: 'link2'}];
+
+        var links = [];
+        await axios('http://localhost:8065/plugins/com.github.shadabk96.mattermost-linkbot-plugin').then((res) => {
+            console.log('1');
+            console.log(res);
+            console.log('2');
+            console.log(res.data);
+            links = res.data;
+            this.setState({links: res.data, loading: false});
+        });
+        console.log('3');
+        console.log(links);
+        console.log('4');
+        console.log(this.state.links);
+    }
+
+    render() {
+        if (this.state.loading) {
+            return 'Loading';
+        }
+        return (
+            <CustomCard
+                data={this.state.links}
+                test={'test'}
+            />
+        );
+    }
+}
